71c7961a02f0ec6472545344e955966bdc08334c
GonzaloSalas1 <alumno21.salas.gonzalo@ipm.edu.ar>
2021-07-15 14:42:36 -0300

    Editar y borrar evento

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { EventoService } from 'src/app/services/evento.service';
 
 @Component({
@@ -8,22 +10,78 @@ import { EventoService } from 'src/app/services/evento.service';
 })
 export class CalendarioComponent implements OnInit {
   eventos: any[] = [];
-  constructor(private _eventoService: EventoService) { }
+
+  editarEvento: FormGroup;
+
+  eventoElegidoEditar: any = "";
+  
+  constructor(private _eventoService: EventoService, private fb: FormBuilder, private router: Router) {
+    this.editarEvento = this.fb.group({
+      nombre: ['', Validators.required],
+      fecha: ['', Validators.required],
+      descripcion: ['', Validators.required]
+    });
+  }
   
 
   ngOnInit(): void {
     this.getEventos();
   }
-getEventos(){
-  this._eventoService.getEventos().subscribe(data => {
-    this.eventos = [];
-    data.forEach((element: any) => {
-      this.eventos.push({
-        id: element.payload.doc.id,
-        ...element.payload.doc.data()
-      })
+
+  getEventos(){
+    this._eventoService.getEventos().subscribe(data => {
+      this.eventos = [];
+      data.forEach((element: any) => {
+        this.eventos.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
     });
-  });
-}
+  }
+
+  mostrarEditar = (opcion: string, eventoId: any) => {
+    const editarContainer: any = document.getElementById(opcion);
+    editarContainer.style.display = 'flex';
+    this.eventoElegidoEditar = eventoId;
+  }
+
+  modificarEvento = (opcion: string) => {
+    let obj = {};
+    
+    if (opcion == 'nombre') {
+      obj = {
+        nombre: this.editarEvento.value.nombre
+      };
+    }
+    else if (opcion == 'fecha') {
+      const fechaEvento: string= new Date(this.editarEvento.value.fecha).toString();
+      obj = {
+        fecha: fechaEvento
+      };
+    }
+    else {
+      obj = {
+        descripcion: this.editarEvento.value.descripcion
+      };
+    }
+
+    this._eventoService.actualizarEvento(this.eventoElegidoEditar, obj).then(() => {
+      console.log("Evento editado");
+      alert("Evento editado");
+      this.router.navigate(['/principal'])
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  eliminarEvento = (eventoId: any) => {
+    this._eventoService.eliminarEvento(eventoId).then(() => {
+      console.log("Evento eliminado");
+      alert("Evento eliminado");
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 }
 
