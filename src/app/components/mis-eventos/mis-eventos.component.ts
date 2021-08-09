bead86f87745e135e1000c9d20df429cda943619
alumno <varella1x@gmail.com>
2021-08-09 11:38:10 -0300

    mis-eventos update

--- src/app/components/mis-eventos/mis-eventos.component.ts
+++ src/app/components/mis-eventos/mis-eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EventoService } from 'src/app/services/evento.service';
 import { OauthService } from 'src/app/services/oauth.service';
 
@@ -11,24 +12,81 @@ export class MisEventosComponent implements OnInit {
 
   misEventos: any = [];
 
-  userId: any = "";
+  eventoElegido: string = "";
 
-  constructor(private _eventoService: EventoService, private _oauth: OauthService) { }
+  opcionElegidaEditar: string = "";
 
-  ngOnInit(): void {
-    this._oauth.getUser().subscribe(data => {
-      this.userId = data.id;
-    });
-    this._eventoService.getEventos().subscribe(res => {
-      for (let evento of res) {
-        if (evento.payload.doc.data().idCreador == this.userId) {
-          this.misEventos.push({
-            id: evento.payload.doc.id,
-            ...evento.payload.doc.data()
-          });
-        }
-      }
+  editarEventoForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private _eventoService: EventoService, private _oauth: OauthService) {
+    this.editarEventoForm = this.fb.group({
+      data: ['', Validators.required]
     });
   }
 
+  ngOnInit(): void {
+    this._oauth.getUser().subscribe(data => {
+      this._eventoService.getEventos().subscribe(res => {
+        for (let evento of res) {
+          if (evento.payload.doc.data().idCreador == data.id) {
+            this.misEventos.push({
+              id: evento.payload.doc.id,
+              ...evento.payload.doc.data()
+            });
+          }
+        }
+      });
+    });
+  }
+
+  mostrarCartelBorrar = (idEvento: string) => {
+    const cartelBorrar: any = document.querySelector(".borrar-cartel-fondo");
+    cartelBorrar.style.display = "flex";
+    this.eventoElegido = idEvento;
+  }
+
+  cerrarCartelBorrar = () => {
+    const cartelBorrar: any = document.querySelector(".borrar-cartel-fondo");
+    cartelBorrar.style.display = "none";
+    this.eventoElegido = "";
+  }
+
+  borrarEvento = () => {
+    const cartelBorrar: any = document.querySelector(".borrar-cartel-fondo");
+    cartelBorrar.style.display = "none";
+    this.eventoElegido = "";
+    this._eventoService.eliminarEvento(this.eventoElegido).then(() => {
+      console.log("Evento borrado correctamente");
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  mostrarCartelEditar = (idEvento: string, opcion: string) => {
+    this.opcionElegidaEditar = opcion;
+    const cartelEditar: any = document.querySelector(".editar-cartel-fondo");
+    cartelEditar.style.display = "flex";
+    this.eventoElegido = idEvento;
+  }
+
+  cerrarCartelEditar = () => {
+    const cartelEditar: any = document.querySelector(".editar-cartel-fondo");
+    cartelEditar.style.display = "none";
+    this.eventoElegido = "";
+    this.opcionElegidaEditar = "";
+  }
+
+  editarEvento = () => {
+    console.log(this.editarEventoForm.value.data);
+    console.log(this.opcionElegidaEditar);
+    const cartelEditar: any = document.querySelector(".editar-cartel-fondo");
+    cartelEditar.style.display = "none";
+    /*this._eventoService.actualizarEvento(this.eventoElegido, {}).then(() => {
+      console.log("Evento editado correctamente");
+    }).catch(error => {
+      console.log(error);
+    })*/
+    this.eventoElegido = "";
+    this.opcionElegidaEditar = "";
+  }
 }
