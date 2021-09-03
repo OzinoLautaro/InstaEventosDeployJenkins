3945fea19a747d9309433e13c9c3cfe77efc467b
gonzalosalas1 <alumno21.salas.gonzalo@ipm.edu.ar>
2021-09-03 11:49:00 -0300

    correccion mercadopago

--- src/app/components/premium/premium.component.ts
+++ src/app/components/premium/premium.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MercadopagoSDk } from 'ngx-mercadopago';
+
+declare const MercadoPago: any;
 
 @Component({
   selector: 'app-premium',
@@ -12,10 +13,58 @@ export class PremiumComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.mostrarBoton();
   }
 
-  realizarPago = (opcion: number) => {
-    console.log(opcion);
+  mostrarBoton = () => {
+    
+    const mp = new MercadoPago('TEST-a535f7cd-0217-4c82-8f48-04793bebfc27', {locale: 'es-AR'});
+
+
+    // Boton del plan 1
+
+    const tokenizer1 = mp.checkout({
+        tokenizer: {
+            totalAmount: 100,
+            backUrl: 'https://localhost:4200/procesar-pago'
+        }
+    });
+
+    tokenizer1.render({
+        container: '.tokenizer-container-1',
+        label: 'Comprar'
+    });
+
+
+    // Boton del plan 2
+
+    const tokenizer2 = mp.checkout({
+        tokenizer: {
+            totalAmount: 200,
+            backUrl: 'https://localhost:4200/procesar-pago'
+        }
+    });
+
+    tokenizer2.render({
+        container: '.tokenizer-container-2',
+        label: 'Comprar'
+    });
+
+
+    // Boton del plan 3
+
+    const tokenizer3 = mp.checkout({
+        tokenizer: {
+            totalAmount: 750,
+            backUrl: 'https://localhost:4200/procesar-pago'
+        }
+    });
+
+    tokenizer3.render({
+        container: '.tokenizer-container-3',
+        label: 'Comprar'
+    });
+
   }
 
 
