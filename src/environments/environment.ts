64eed7513c2e4136924e36159fb47fb8f090a179
gonzalosalas1 <alumno21.salas.gonzalo@ipm.edu.ar>
2021-10-12 16:13:10 -0300

    Github Actions - Firebase Deploy Test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -25,7 +25,7 @@ export const environment = {
 export const CLIENT_ID = "844270250118873128";
 export const CLIENT_SECRET = "4kFQc5VFURNtbmSFWO5qb9a5Uj0iDli4";
 export const REDIRECT_URI = "https://discordbot-946b9.web.app/login";
-export const DISCORD_LOGIN_URL = "https://discord.com/api/oauth2/authorize?client_id=844270250118873128&redirect_uri=https%3A%2F%2Fdiscordbot-946b9.web.app%2F&response_type=code&scope=identify%20connections%20guilds";
+export const DISCORD_LOGIN_URL = "https://discord.com/api/oauth2/authorize?client_id=844270250118873128&redirect_uri=https%3A%2F%2Fdiscordbot-946b9.web.app%2Flogin&response_type=code&scope=identify%20connections%20guilds";
 export const DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token";
 
 export const MP_PUBLIC_KEY = 'APP_USR-a9dfe689-b97a-4ea5-b65d-4e1e0d3f2b84';
